63254c61e48558d4fb26b29a6a9bcf603c773b0d
jakhongirov <johongirovdiyorbek08@gmail.com>
2024-01-23 16:31:31 +0500

    change port

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const PORT = 9000;
+const PORT = 7000;
 const connection = {
    connectionStringEL: "postgres://sgeengir:U9FyXq00U5YN1h-ud-HRTc9aIC_0pSuA@tai.db.elephantsql.com/sgeengir",
    // connectionStringEL: "postgres://jvesrinh:nH4LbnttMxxtujfDFolGSMp2pTt1Ro7Z@tyke.db.elephantsql.com/jvesrinh",
