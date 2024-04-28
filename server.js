8cbbb03662a11f68a559d4bcff5d9ad88995bb03
jakhongirov <johongirovdiyorbek08@gmail.com>
2024-04-28 15:55:48 +0500

    add

--- server.js
+++ server.js
@@ -288,9 +288,9 @@ const options = {
    definition: {
       openapi: "3.0.0",
       info: {
-         title: "Library API",
+         title: "Qiblah API documentation",
          version: "1.0.0",
-         description: "A simple Express Library API",
+         description: "by Diyor Jaxongirov",
       },
       servers: [
          {
