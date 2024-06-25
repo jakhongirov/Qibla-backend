111c1c1fd2695ccb8a0def712a88261db7f7f73a
jakhongirov <johongirovdiyorbek08@gmail.com>
2024-06-25 16:41:43 +0500

    add

--- src/modules/click/click.js
+++ src/modules/click/click.js
@@ -24,6 +24,7 @@ module.exports = {
             }
             const timestamp = expiresDate.getTime();
 
+            console.log(expiresDate)
             console.log(timestamp)
 
             await model.editUserPremium(param2, timestamp)
